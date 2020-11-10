09146986b8722d4ba12e378d7b824b218343a48a
Valentine <marlosgama@outlook.com>
2020-11-10 13:49:20 -0300

    Update word_wrap.rb

--- word_wrap.rb
+++ word_wrap.rb
@@ -15,38 +15,38 @@ class Window_Base < Window
     return [text] if bitmap.text_size(text).width <= width && !text.include?("\n")
     lines = []
     line = ''
-    line_size = 0
+    line_width = 0
     text.each_line(' ') do |word|
-      word_size = bitmap.text_size(word).width
+      word_width = bitmap.text_size(word).width
       if word.include?("\n")
-        line, lines, line_size = skip_line(word, width, bitmap, line, lines, line_size)
-      elsif word_size > width
+        line, lines, line_width = skip_line(word, width, bitmap, line, lines, line_width)
+      elsif word_width > width
         line, lines = character_wrap(word, width, bitmap, line, lines)
-      elsif line_size + word_size <= width
+      elsif line_width + word_width <= width
         line << word
-        line_size += word_size
+        line_width += word_width
       else
         lines << line
         line = word
-        line_size = word_size
+        line_width = word_width
       end
     end
     bitmap.dispose unless contents
     lines << line
   end
   
-  def skip_line(words, width, bitmap, line, lines, line_size)
+  def skip_line(words, width, bitmap, line, lines, line_width)
     words.each_line do |word|
       # Se a última palavra da matriz não está
       #acompanhada do comando de quebra de linha
       unless word.end_with?("\n")
         line = word
-        line_size = bitmap.text_size(word).width
+        line_width = bitmap.text_size(word).width
         break
       end
       word = word.delete("\n")
-      word_size = bitmap.text_size(word).width
-      if line_size + word_size <= width
+      word_width = bitmap.text_size(word).width
+      if line_width + word_width <= width
         # Impede que as linhas sejam alteradas quando a
         #linha atual for apagada
         lines << line.clone + word
@@ -54,24 +54,24 @@ class Window_Base < Window
         lines << line.clone << word
       end
       line.clear
-      line_size = 0
+      line_width = 0
     end
-    return line, lines, line_size
+    return line, lines, line_width
   end
   
   def character_wrap(word, width, bitmap, line, lines)
     cs = ''
-    cs_size = 0
+    cs_width = 0
     word.each_char do |c|
-      c_size = bitmap.text_size(c).width
-      if cs_size + c_size <= width
+      c_width = bitmap.text_size(c).width
+      if cs_width + c_width <= width
         cs << c
-        cs_size += c_size
+        cs_width += c_width
       else
         lines << line.clone unless line.empty?
         lines << cs
         cs = c
-        cs_size = c_size
+        cs_width = c_width
         line.clear
       end
     end
