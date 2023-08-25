464c5683a3d09cbf9a636a24d5270df20113826b
Valentine <marlosgama@outlook.com>
2023-08-25 19:18:10 -0300

    Update word_wrap.rb

--- word_wrap.rb
+++ word_wrap.rb
@@ -15,7 +15,6 @@ class Window_Base < Window
     lines = []
     line = ''
     line_width = 0
-
     text.each_line(' ') do |word|
       word_width = bitmap.text_size(word).width
       if word.include?("\n")
@@ -31,9 +30,7 @@ class Window_Base < Window
         line_width = word_width
       end
     end
-
     bitmap.dispose unless contents
-
     lines << line
   end
 
@@ -46,10 +43,8 @@ class Window_Base < Window
         line_width = bitmap.text_size(word).width
         break
       end
-  
       word = word.delete("\n")
       word_width = bitmap.text_size(word).width
-
       if line_width + word_width <= width
         # Impede que as linhas sejam alteradas quando a
         # linha atual for apagada.
@@ -57,18 +52,15 @@ class Window_Base < Window
       else
         lines << line.clone << word
       end
-
       line.clear
       line_width = 0
     end
-
     return line, lines, line_width
   end
 
   def character_wrap(word, width, bitmap, line, lines)
     cs = ''
     cs_width = 0
-
     word.each_char do |c|
       c_width = bitmap.text_size(c).width
       if cs_width + c_width <= width
@@ -82,7 +74,6 @@ class Window_Base < Window
         line.clear
       end
     end
-
     return line << cs, lines
   end
 end
